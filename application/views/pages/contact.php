902cf853b370c802e1b23d695f71207088ae5ef7
Riz Michael Sim <rizsim214@gmail.com>
2022-02-11 19:03:30 +0800

    Fourth Commit

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,8 +1,8 @@
 <section id="contact" class="container-fluid">
     <div class="col-md-4 mx-auto mb-auto mt-5">
-        <div class="my-5">
-            <h3 class="text-center mt-5">Contact Us</h3>
-            <p class="text-muted text-center">How can we be of service?</p>
+        <div class="mb-3">
+            <h3 class="text-center mt-5">Inquiry</h3>
+            <p class="text-muted text-center">How can we help you?</p>
         </div>
         <?= form_open();?>
         
@@ -25,9 +25,10 @@
             <div class="form-group my-3">
             <?= form_textarea($data = array(
                     'name' => 'message',
-                    'value' => 'Send us a message...',
-                    'class' => 'form-control'
-                    
+                    'placeholder' => 'Send us a message...',
+                    'class' => 'form-control',
+                    'rows' => '5',
+                    'cols' => '50'
                 ));?>
             </div>
             <?= form_submit($data = array(
